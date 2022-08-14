b252571c293c64ad5586ae5fef04536a198fe251
Luca Carrara <luca.carrara00@gmail.com>
2022-08-14 10:49:49 +0200

    Remote Control Car exercise completed

---
+++ 27_RemoteControlCar/RemoteControlCar.ex
@@ -0,0 +1,26 @@
+defmodule RemoteControlCar do
+  @enforce_keys [:nickname]
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
+
+  def new(nickname \\ "none"), do: %RemoteControlCar{nickname: nickname}
+
+  def display_distance(%RemoteControlCar{distance_driven_in_meters: distance}),
+    do: "#{distance} meters"
+
+  def display_battery(%RemoteControlCar{battery_percentage: 0}), do: "Battery empty"
+
+  def display_battery(%RemoteControlCar{battery_percentage: battery}),
+    do: "Battery at #{battery}%"
+
+  def drive(%RemoteControlCar{battery_percentage: 0} = remote_car), do: remote_car
+
+  def drive(
+        %RemoteControlCar{battery_percentage: battery, distance_driven_in_meters: distance} =
+          remote_car
+      ),
+      do: %{
+        remote_car
+        | battery_percentage: battery - 1,
+          distance_driven_in_meters: distance + 20
+      }
+end
